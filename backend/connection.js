c34e4f42ffb50d7f805b07983525325da3de6def
Nicoov <nicolas_ignacio_1@hotmail.com>
2023-04-05 16:03:32 -0400

    update comentarios backend

--- backend/connection.js
+++ backend/connection.js
@@ -5,11 +5,13 @@ const bodyParser = require('body-parser');
 
 
 const app = express();
+
+//middlewares 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
+//conexion a la base de datos
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -19,6 +21,8 @@ const db = mysql.createConnection({
 
 
 
+
+// peticion GET para todos los usuarios
 app.get('/usuarios', (req, res) => {
     db.query("SELECT * FROM usuarios", (err, rows, field) => {
         if (!err) {
@@ -30,7 +34,7 @@ app.get('/usuarios', (req, res) => {
 });
 
 
-
+// peticion POST para registrar usuarios
 app.post('/register', (req, res) => {
     const { nombre, email, informacion, password } = req.body;
     const sql = 'INSERT INTO usuarios (nombre, email, informacion ,password) VALUES (?, ?, ?,?)';
@@ -46,9 +50,7 @@ app.post('/register', (req, res) => {
 
 
 
-
-
-
+//Peticion post para hacer login donde se hace una query sql para validar si el usuario tiene el email y password
 app.post('/login', (req, res) => {
     const { email, password } = req.body;
     const values = [req.body.email, req.body.password]
@@ -69,7 +71,7 @@ app.post('/login', (req, res) => {
 });
 
 
-
+// Peticion DELETE donde se hace una query para eliminar el usuario dependiendo el id
 app.delete("/usuarios/:id", (req, res) => {
     const userId = req.params.id
     const sql = 'DELETE FROM usuarios WHERE id = ?';
@@ -82,6 +84,8 @@ app.delete("/usuarios/:id", (req, res) => {
     })
 })
 
+
+// Peticion para editar usuarios donde se hace una query para actualizar usuarios nombre e informacion con el id
 app.put("/usuarios/:id", (req, res) => {
     const userId = req.params.id
     const values = [
